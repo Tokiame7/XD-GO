e602e8ace660931f15442c2b4a48c7f86d73e9fe
miku <miku@q.com>
2025-03-29 18:34:08 +0800

    修改订单状态

--- frontend/src/views/seller/seller_order/index.vue
+++ frontend/src/views/seller/seller_order/index.vue
@@ -1,29 +1,54 @@
 <template>
-<template v-if="loading">
+  <div>
+  <template v-if="loading">
     <p>数据加载中，请稍候...</p>
   </template>
-<template v-else>
+  <template v-else>
     <el-table :data="OrderList" stripe style="width: 100%">
       <el-table-column prop="orderid" label="订单id" width="180" />
       <el-table-column prop="totalprice" label="总价格" width="180" />
       <el-table-column prop="status" label="status" width ='180' />
       <el-table-column prop="createtime" label="创建时间" />
+      <el-table-column fixed="right" label="操作" min-width="120">
+        <template #default="scope">
+        <el-button
+          link
+          type="primary"
+          size="small"
+          @click.prevent="Editorder(scope.row.orderid,scope.row.status)"
+        >
+          发货
+        </el-button>
+      </template>
+
+
+
+      </el-table-column>
     </el-table>
+
   </template>
+  </div>
 </template>
 <script setup>
 
-import { useGetOrder } from '@/stores/seller_products';
+import { shipOrder } from '@/api/seller';
+import { useGetOrder,useShiporder } from '@/stores/seller_products';
 import { ref ,reactive ,watchEffect} from 'vue'
 const loading = ref(true);//加载对象
 const OrderList = ref([])
 const GetOrder = useGetOrder()
+const Shiporder = useShiporder()
 
 //整个组件挂载后的行为
 onMounted(() => {
   GetOrder.getorders();
   OrderList.value = GetOrder.orderList;
 });
+
+const Editorder = (id,status)=>{
+  console.log(id,status)
+  Shiporder.shipOrderstatus(id,status);
+}
 
 //监听数据变化同步数据变化
 watchEffect(() => {
