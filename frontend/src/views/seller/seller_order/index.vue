78ec4cb6c8d624f750157848e262cc9306e168b1
miku <miku@q.com>
2025-03-30 15:36:01 +0800

    更改yingwen

--- frontend/src/views/seller/seller_order/index.vue
+++ frontend/src/views/seller/seller_order/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
   <template v-if="loading">
-    <p>数据加载中，请稍候...</p>
+    <p>Pageloading,waiting...</p>
   </template>
   <template v-else>
     <el-table :data="OrderList" stripe style="width: 100%">
-      <el-table-column prop="orderid" label="订单id" width="180" />
-      <el-table-column prop="totalprice" label="总价格" width="180" />
+      <el-table-column prop="orderid" label="OrderId" width="180" />
+      <el-table-column prop="totalprice" label="TotalPrice" width="180" />
       <el-table-column prop="status" label="status" width ='180' />
-      <el-table-column prop="createtime" label="创建时间" />
-      <el-table-column fixed="right" label="操作" min-width="120">
+      <el-table-column prop="createtime" label="CreateTime" />
+      <el-table-column fixed="right" label="Options" min-width="120">
         <template #default="scope">
         <el-button
           link
@@ -17,7 +17,7 @@
           size="small"
           @click.prevent="Editorder(scope.row.orderid,scope.row.status)"
         >
-          发货
+          Deliver
         </el-button>
       </template>
 
