2580732033ee67e4dd6d59edc247189975fa4ef5
miku <miku@q.com>
2025-04-05 16:26:16 +0800

    卖家获取分类id修改

--- frontend/src/views/seller/seller_addproducts/index.vue
+++ frontend/src/views/seller/seller_addproducts/index.vue
@@ -46,82 +46,73 @@
     <!-- 弹出添加商品 -->
     <el-dialog v-model="dialogVisible" :modal="false">
       <!-- 模态框主内容 -->
-    <el-form :model="form" label-width="auto" style="max-width: 600px">
-      <!-- 第一个框 -->
-    <el-form-item label="ProductName*">
-      <el-input v-model="form.name" />
-    </el-form-item>
-    <el-form-item label="Description">
-      <el-input v-model="form.description" />
-    </el-form-item>
-    <el-form-item label="Price*(>0)">
-      <el-input v-model.number="form.price" />
-    </el-form-item>
-    <el-form-item label="Stock*(>0)">
-      <el-input v-model.number="form.stock" />
-    </el-form-item>
-    <el-form-item label="Imageurl">
-      <el-input v-model="form.image" />
-    </el-form-item>
-    <el-form-item label="catid*">
-      <el-input v-model="form.catid" />
-      <text>cat_clothing、</text>
-      <text>cat_electronics</text>
-    </el-form-item>
-     <!-- 选项框 -->
-    <!-- <el-form-item label="商品分类*">
-      <el-checkbox-group v-model="form.type">
-        <el-checkbox value="Online activities" name="type">
-          Clothing
-        </el-checkbox>
-        <el-checkbox value="Promotion activities" name="type">
-          Electronics
-        </el-checkbox>
-      </el-checkbox-group>
-    </el-form-item> -->
-<!-- 多内容框 /文件框-->
-    <el-form-item label="Other">
-      <el-input v-model="form.desc" type="textarea" />
-    </el-form-item>
+      <el-form :model="form" label-width="auto" style="max-width: 600px">
+        <!-- 第一个框 -->
+      <el-form-item label="ProductName*">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="Description">
+        <el-input v-model="form.description" />
+      </el-form-item>
+      <el-form-item label="Price*(>0)">
+        <el-input v-model.number="form.price" />
+      </el-form-item>
+      <el-form-item label="Stock*(>0)">
+        <el-input v-model.number="form.stock" />
+      </el-form-item>
+      <el-form-item label="Imageurl">
+        <el-input v-model="form.image" />
+      </el-form-item>
+      <el-form-item label="catid*">
+        <el-input v-model="form.catid" />
+        <div>Can Choose:</div>
+        <div v-for="cat in catId" :key="cat.categoryId" class="Catids">
+          <div>
+            {{ cat.categoryName }}、
+          </div>
+        </div>
+      </el-form-item>
+            <!-- 多内容框 /文件框-->
+      <el-form-item label="Other">
+        <el-input v-model="form.desc" type="textarea" />
+      </el-form-item>
 
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">Create</el-button>
-      <el-button @click="resetForm">Delete</el-button>
-    </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Create</el-button>
+        <el-button @click="resetForm">Delete</el-button>
+      </el-form-item>
 
-  </el-form>
-    <!-- 底部按键 -->
-    <template #footer>
-      <div class="dialog-footer">
-        <el-button @click="dialogVisible = false">esc</el-button>
-        <el-button type="primary" @click="dialogVisible = false">
-          confirm
-        </el-button>
-      </div>
-    </template>
-  </el-dialog>
+      </el-form>
+      <!-- 底部按键 -->
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogVisible = false">esc</el-button>
+          <el-button type="primary" @click="dialogVisible = false">
+            confirm
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
+
   </div>
 </template>
 
 <script setup>
 import { ref, reactive, onMounted , watchEffect } from 'vue';
-import { useSellerProucts,useDeleteProduct,useAddProduct } from '@/stores/seller_products';
+import { useSellerProucts,useDeleteProduct,useAddProduct,useGetshopCatid } from '@/stores/seller_products';
+import { get } from 'lodash-es';
 
-const params = ref({
-  page: 1,
-  pageSize: 10,
-  search: '',
-  sellerid: 'seller_001'
-});
 
 // 先创建实例
 const sellerProducts = useSellerProucts(); 
 const addproduct = useAddProduct();
 const deleproduct = useDeleteProduct();
+const getshopCatid = useGetshopCatid();
 
 const loading = ref(true);//加载对象
 const products = ref([])//当前响应式对象
 const dialogVisible = ref(false)//模态框状态
+const catId = ref([])//所有catid
 //初始化的空表单
 const initialform = {
   name: '',
@@ -157,14 +148,22 @@ const resetForm = () => {
 //整个组件挂载后的行为
 onMounted(() => {
   sellerProducts.getSellerProductsList();
+  getshopCatid.getcatid();
 });
-
+onMounted(() => {
+  
+  
+});
 //监听数据变化同步数据变化 立即执行一次
 watchEffect(() => {
-    console.log('sellerProductsList 发生变化:', sellerProducts.sellerProductsList);
     if (sellerProducts.sellerProductsList.length > 0) {
+        console.log('sellerProductsList 发生变化:', sellerProducts.sellerProductsList);
         products.value = sellerProducts.sellerProductsList;
         loading.value = false;//不要忘记加载
+    }
+    if(getshopCatid.catidList.length > 0){
+      console.log('catidList 发生变化:', getshopCatid.catidList);
+      catId.value = getshopCatid.catidList;
     }
 });
 
